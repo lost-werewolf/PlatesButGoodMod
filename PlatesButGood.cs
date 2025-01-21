01d8340f8d4cb7de69b6ac948d307820e169958d
lost-werewolf <sebmartinpersonal@gmail.com>
2025-01-21 17:50:44 -0500

    Animated item fix courtesy of jamzojamz
    how to rotate text in ms paint

--- PlatesButGood.cs
+++ PlatesButGood.cs
@@ -1,11 +1,17 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria;
+using Terraria.GameContent.Drawing;
 using Terraria.GameContent.Tile_Entities;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace PlatesButGood
@@ -14,29 +20,47 @@ namespace PlatesButGood
 	{
         public override void Load()
         {
-            IL_TEFoodPlatter.FitsFoodPlatter += HookFitsFoodPlatter;
+            On_TEFoodPlatter.FitsFoodPlatter += (_, _) => true;
+            IL_TileDrawing.DrawSpecialTilesLegacy += IL_TileDrawingOnDrawSpecialTilesLegacy;
         }
 
-        private static void HookFitsFoodPlatter(ILContext il)
+        private void IL_TileDrawingOnDrawSpecialTilesLegacy(ILContext il)
         {
-            try {
-                ILCursor c = new ILCursor(il);
+            try
+            {
+                // Start the Cursor at the start
+                var c = new ILCursor(il);
 
-                c.GotoNext(i => i.MatchLdcI4(0));
+                // Try to locate where to perform the IL edit
+                c.GotoNext(i => i.MatchLdsfld<ItemID.Sets>("IsFood"));
 
-                c.Index += 2; // Jump to after ble.s
+                c.EmitLdloc(24); // Load item3 local var onto stack (Item)
+                c.EmitLdloc(25); // Load value3 local var on stack (Texture2D)
 
-                c.Emit(OpCodes.Ldc_I4_1); // Setup returning true
+                c.EmitDelegate<Func<Item, Texture2D, Rectangle>>((item, texture) =>
+                    !ItemID.Sets.IsFood[item.type]
+                        ? Main.itemAnimations[item.type] == null
+                            ? texture.Frame()
+                            : Main.itemAnimations[item.type].GetFrame(texture)
+                        : texture.Frame(1, 3, 0, 2));
 
-                c.Emit(OpCodes.Ret); // Return true
+                // Save current cursor position
+                var index = c.Index;
+
+                // Locate the next stloc.s instruction
+                c.GotoNext(i => i.MatchStloc(out _));
+                var insn = c.Next!;
+
+                // Return to the saved cursor position
+                c.Index = index;
+
+                // Emit break instruction
+                c.EmitBr(insn);
             }
-            catch (Exception e) {
-                MonoModHooks.DumpIL(ModContent.GetInstance<PlatesButGood>(), il);
+            catch
+            {
+                MonoModHooks.DumpIL(this, il);
             }
-        }
-        public override void Unload()
-        {
-            IL_TEFoodPlatter.FitsFoodPlatter -= HookFitsFoodPlatter;
         }
     }
 }
